a1f443af8a6e64745258221f3358df6852554e6e
Gabriel Acuña <gstef09@hotmail.com>
2016-02-01 22:50:18 -0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sistemanotas/';
+$config['base_url'] = 'http://localhost/SistemaNotas/';
 
 /*
 |--------------------------------------------------------------------------
